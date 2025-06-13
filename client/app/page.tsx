1c8f886562598386213c0db75d48707e4ca15cac
Austinosteph@gmail.com <Austinosteph@gmail.com>
2025-06-13 14:10:24 +0100

    feature-hero

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,5 +1,15 @@
+import Hero from "@/components/hero";
 const Home = () => {
-  return <main className="min-h-screen">This is the Home</main>;
+  return (
+    <main className="min-h-screen">
+      <Hero />
+      <div>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit quam
+        velit esse enim illum. Ducimus esse eum cumque incidunt excepturi. Animi
+        vero quam enim in eligendi quos necessitatibus eveniet maiores.
+      </div>
+    </main>
+  );
 };
 
 export default Home;
--- client/public/index.ts
+++ client/public/index.ts
@@ -1,7 +1,9 @@
 import logo from "./logo.svg";
 import logo_white from "./logo_white.svg";
+import hero from "./hero.png";
 
 export const assets = {
   logo,
   logo_white,
+  hero,
 };
