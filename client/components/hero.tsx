76f2f216cec538000bea7230fdfcbcd8dc9c091f
Austinosteph@gmail.com <Austinosteph@gmail.com>
2025-06-14 09:09:58 +0100

    updated hero page

--- client/components/hero.tsx
+++ client/components/hero.tsx
@@ -44,10 +44,8 @@ const Hero = () => {
               className="w-96 h-full"
             />
             <div className="max-w-96 w-80 space-y-4">
-              <p className="font-medium text-5xl text-black leading-10">
-                Playstation 5
-              </p>
-              <p className="font-medium text-sm opacity-60 leading-6 pr-4">
+              <p className="font-medium text-5xl leading-10">Playstation 5</p>
+              <p className="font-medium text-sm opacity-70 leading-6 pr-4">
                 Incredibly powerful CPUs, GPUs, and an SSD with integrated I/O
                 will redefine your PlayStation experience.
               </p>
@@ -62,11 +60,11 @@ const Hero = () => {
                 width={104}
                 className="w-28 h-full"
               />
-              <div className="max-w-96 w-80 space-y-4">
-                <p className="font-light text-3xl text-black leading-10">
+              <div className="w-40 h-32 pl-4">
+                <p className="font-light text-3xl leading-10">
                   Apple AirPods <span className="font-medium">Max</span>
                 </p>
-                <p className="font-medium text-sm opacity-60 leading-6 pr-4">
+                <p className="font-medium text-sm opacity-70 leading-6 pr-4">
                   Computational audio. Listen, it's powerful
                 </p>
               </div>
@@ -75,22 +73,46 @@ const Hero = () => {
               <Image
                 src={assets.applevission}
                 alt="airpod"
-                height={272}
-                width={104}
-                className="w-28 h-full"
+                height={190}
+                width={136}
+                className="w-36 h-48"
               />
-              <div className="max-w-96 w-80 space-y-4 text-white">
-                <p className="font-light text-3xl text-black leading-10">
-                  Apple AirPods <span className="font-medium">Max</span>
+              <div className="w-40 h-20 text-white pl-4">
+                <p className="font-light text-3xl leading-10">
+                  Apple Vision <span className="font-medium">Pro</span>
                 </p>
-                <p className="font-medium text-sm opacity-60 leading-6 pr-4">
-                  Computational audio. Listen, it's powerful
+                <p className="font-medium text-sm opacity-50 leading-6">
+                  An immersive way to experience entertainment
                 </p>
               </div>
             </div>
           </div>
         </div>
-        <div className="bg-green-200">2</div>
+
+        <div className="bg-gray-200 flex items-center justify-center">
+          <div className="grid gap-4 w-96 max-w-80">
+            <p className="font-[100] text-6xl leading-10">
+              Macbook <span className="font-medium">Air</span>
+            </p>
+            <p className="font-medium text-sm opacity-70 leading-6">
+              The new 15‑inch MacBook Air makes room for more of what you love
+              with a spacious Liquid Retina display.
+            </p>
+            <Link
+              href="/"
+              className="border w-fit rounded-md p-4 px-10 font-medium text-base leading-6"
+            >
+              shop now
+            </Link>
+          </div>
+          <Image
+            src={assets.macbook}
+            alt="macbook"
+            height={502}
+            width={292}
+            className="w-72 h-[502px]"
+          />
+        </div>
       </section>
     </main>
   );
