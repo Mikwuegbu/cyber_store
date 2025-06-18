3777acb5f3b7b983f0311d895c0024e976449681
Austinosteph@gmail.com <Austinosteph@gmail.com>
2025-06-18 02:56:44 +0100

    feat: add product showcase components and images for e-commerce display

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,13 @@
 import Categories from "@/components/categories";
 import Hero from "@/components/hero";
+import Productshowcase from "@/components/productshowcase";
 const Home = () => {
   return (
     <main className="min-h-screen">
       <Hero />
       <Categories />
+      <Productshowcase />
+
       <div>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit quam
         velit esse enim illum. Ducimus esse eum cumque incidunt excepturi. Animi
